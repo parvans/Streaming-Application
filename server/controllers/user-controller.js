63f5228dae3a83ee4044faf3094790a6b7683985
Parvan S <85297690+parvans@users.noreply.github.com>
2023-02-07 12:46:31 +0530

    Update user-controller.js

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -31,6 +31,7 @@ const registerUser = async (req, res) => {
     }
 }
 
+
 const loginUser = async (req, res) => {
     const { email, password } = req.body
     let user = await User.findOne({ email })
